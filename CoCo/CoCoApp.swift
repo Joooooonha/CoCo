2b296f969579bed441395ce9422fc9dfe6e1fc08
Joooooonha <wnsgk517@naver.com>
2026-07-22 21:09:37 +0900

    Initial Commit

---
+++ CoCo/CoCoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoCoApp.swift
+//  CoCo
+//
+//  Created by 박준하 on 7/22/26.
+//
+
+import SwiftUI
+
+@main
+struct CoCoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoCo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoCo
+//
+//  Created by 박준하 on 7/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
